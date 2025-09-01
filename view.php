38b9159468c4961881ba6d6d93e90399bc6afa97
Hemalatha-code-bit <shettigar1995@gmail.com>
2025-09-01 16:36:36 +0530

    Update view.php fixed XSS

--- view.php
+++ view.php
@@ -5,6 +5,6 @@ $msg = isset($_GET['msg']) ? $_GET['msg'] : '';
 <!doctype html>
 <html><body>
   <h1>Message Viewer</h1>
-  <div>Message: <?php echo $msg; /* reflected XSS - intentionally unescaped */ ?></div>
+  <div>Message: <?php echo htmlspecialchars($msg, ENT_QUOTES, 'UTF-8'); ?></div>
   <p><a href="index.php">Home</a></p>
 </body></html>
